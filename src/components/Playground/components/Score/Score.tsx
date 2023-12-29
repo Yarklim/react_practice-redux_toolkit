8697668e20e3df8ea61e913a263eac1119bc82bc
Yaroslav Klimas <yarklim@gmail.com>
2023-12-29 13:41:04 +0200

    added Score

---
+++ src/components/Playground/components/Score/Score.tsx
@@ -0,0 +1,16 @@
+import { useAppSelector } from "../../../../app/hooks"
+
+const Score: React.FC = () => {
+  const state = useAppSelector((state) => state.playground)
+
+  return (
+    <div>
+      <h3>Score</h3>
+      <span>Errors: {state.totalUnsuccessful}</span>
+      <br />
+      <span>Successful: {state.totalSuccessful}</span>
+    </div>
+  )
+}
+
+export default Score
