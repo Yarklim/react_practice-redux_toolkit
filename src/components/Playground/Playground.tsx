dd7878db40b67443db0db13da966b32b69260e52
Yaroslav Klimas <yarklim@gmail.com>
2023-12-30 11:50:24 +0200

    added Description

--- src/components/Playground/Playground.tsx
+++ src/components/Playground/Playground.tsx
@@ -10,6 +10,7 @@ import RandomKeys from "./components/RandomKeys"
 import KeyPressed from "./components/KeyPressed"
 import Score from "./components/Score"
 import Modal from "./components/Modal"
+import Description from "./components/Description"
 
 const Playground: React.FC = () => {
   const state = useAppSelector((state) => state.playground)
@@ -62,6 +63,7 @@ const Playground: React.FC = () => {
       <RandomKeys isTimerActive={isTimerActive} />
       <KeyPressed isTimerActive={isTimerActive} />
       <Score />
+      <Description />
       {isShowModal && <Modal setIsShowModal={setIsShowModal} isWin={isWin} />}
     </div>
   )
