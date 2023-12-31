c5f389e3735133f1531b3ae5edfabd52d6426b4d
Yaroslav Klimas <yarklim@gmail.com>
2023-12-31 17:10:19 +0200

    updated vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react_practice-redux_toolkit/",
+  base: "react_practice-redux_toolkit",
   server: {
     open: true,
   },
